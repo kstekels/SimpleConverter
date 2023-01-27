93fdaf1cc0d10582ecf787cf6d15fc85757b1894
Karlis <karlisstekels.dev@gmail.com>
2023-01-27 08:15:17 +0200

    Initial Commit

---
+++ UnitTestDemoApp/UnitTestDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTestDemoAppApp.swift
+//  UnitTestDemoApp
+//
+//  Created by karlis.stekels on 27/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct UnitTestDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
